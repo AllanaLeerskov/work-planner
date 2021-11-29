014d71730a2ea9f1eda844feee3c2ca8d27513c9
Elektra <allanaleerskov@gmail.com>
2021-11-28 22:24:16 -0700

    .getItem for each timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,35 @@ var interval = setInterval(function(){
 }, 100);
 
 //show on page
+function initPage() {
+    var init9 = JSON.parse(localStorage.getItem("09:00am"));
+    nineAm.val(init9);
+
+    var init10 = JSON.parse(localStorage.getItem("10:00am"));
+    tenAm.val(init10);
+
+    var init11 = JSON.parse(localStorage.getItem("11:00am"));
+    elevenAm.val(init11);
+
+    var init12 = JSON.parse(localStorage.getItem("12:00pm"));
+    twelvePm.val(init12);
+
+    var init1 = JSON.parse(localStorage.getItem("01:00pm"));
+    onePm.val(init1);
+
+    var init2 = JSON.parse(localStorage.getItem("02:00pm"));
+    twoPm.val(init2);
+
+    var init3 = JSON.parse(localStorage.getItem("03:00pm"));
+    threePm.val(init3);
+
+    var init4 = JSON.parse(localStorage.getItem("04:00pm"));
+    fourPm.val(init4);
+
+    var init5 = JSON.parse(localStorage.getItem("05:00pm"));
+    fivePm.val(init5);
+
+    var init6 = JSON.parse(localStorage.getItem("06:00pm"));
+    sixPm.val(init6);
+};
+
