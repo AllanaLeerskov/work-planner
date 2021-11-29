2ff9bba681c4319760573779d163732c4b206287
Elektra <allanaleerskov@gmail.com>
2021-11-28 23:26:38 -0700

    changed ids to military, set local, set clear local, fixed moment bugs by reformatting, debugged typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,104 @@
-//timeblock vars
+// Time block variables
 var nineAm = $("#9am");
 var tenAm = $("#10am");
 var elevenAm = $("#11am");
 var twelvePm = $("#12pm");
-var onePm = $("#1pm");
-var twoPm = $("#2pm");
-var threePm = $("#3pm");
-var fourPm = $("#4pm");
-var fivePm = $("#5pm");
-var sixPm = $("#6pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+//military time for id's
 
-// moment.js vars and formatting
+// moment formatting and variables
+var currentDate = moment().format('dddd') + " " + moment().format("DD MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
-var currentDate = moment().format('dddd') + " " + moment.format("DD MM YYYY");
 
-var hourBlock = moment().hours();
-var hourPass;
+
+var hour = moment().hours();
 var userInput;
+var hourSpan;
 
-// date and hour displayed on homepage
+//display time on homepage
 
-var interval = setInterval(function(){
-    var momentNow = moment();
-    $("#currentDay").html(momentNow.format("YYYY MM DD") + " " + momentNow.format("dddd").substring(0,3).toUpperCase());
-    $("#currentDay").html(currentDate + " " + momentNow.format("hh:mm:ss A"));
+var interval = setInterval(function() {
+  var momentNow = moment();
+  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' ' + momentNow.format('dddd').substring(0,3).toUpperCase());
+  $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
 }, 100);
 
-//show on page
 function initPage() {
-    var init9 = JSON.parse(localStorage.getItem("09:00am"));
-    nineAm.val(init9);
 
-    var init10 = JSON.parse(localStorage.getItem("10:00am"));
-    tenAm.val(init10);
+  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nineAm.val(init9);
 
-    var init11 = JSON.parse(localStorage.getItem("11:00am"));
-    elevenAm.val(init11);
+  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+  tenAm.val(init10);
+  
+  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+  elevenAm.val(init11);
+  
+  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+  twelvePm.val(init12);
+  
+  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+  onePm.val(init1);
+  
+  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(init2);
+  
+  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(init3);
+ 
+  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(init4);
+  
+  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(init5);
+  
+  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+  sixPm.val(init6);
+  
+  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+  sevenPm.val(init7);
+} 
 
-    var init12 = JSON.parse(localStorage.getItem("12:00pm"));
-    twelvePm.val(init12);
+function background () {
+      
+  $(".form-control").each(function () {
+      var timeTest = parseInt($(this).attr("id"));
+      hour = parseInt(hour);
+    //   console.log(timeTest);
+    //   console.log(hour);
 
-    var init1 = JSON.parse(localStorage.getItem("01:00pm"));
-    onePm.val(init1);
+      if (hour > timeTest) {
+          $(this).addClass("past");
+      } else if (hour < timeTest) {
+          $(this).addClass("future");
+      } else {
+          $(this).addClass("present");
+      }
+  });
+}
 
-    var init2 = JSON.parse(localStorage.getItem("02:00pm"));
-    twoPm.val(init2);
+$(document).ready(function(){
+  initPage()
+  background()
 
-    var init3 = JSON.parse(localStorage.getItem("03:00pm"));
-    threePm.val(init3);
+  // local storage when btn clicked
+  $(".saveBtn").on("click", function(){
+    userInput = $(this).siblings(".form-control").val().trim();
+    console.log(userInput);
+    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+    console.log(hourSpan);
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
 
-    var init4 = JSON.parse(localStorage.getItem("04:00pm"));
-    fourPm.val(init4);
+  })
+  // delete local storage info when btn clicked
+  $("#clearDay").on("click", function(){
+    localStorage.clear();
+    initPage()
+  }) 
 
-    var init5 = JSON.parse(localStorage.getItem("05:00pm"));
-    fivePm.val(init5);
-
-    var init6 = JSON.parse(localStorage.getItem("06:00pm"));
-    sixPm.val(init6);
-};
-
+});
